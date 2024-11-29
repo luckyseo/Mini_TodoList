73c2b7b3de9505c22a456dce73c133d69841e9f8
luckyseo <ityun777@gmail.com>
2024-11-29 15:14:01 +1100

    toDoList -> localStorage

---
+++ js/todo.js
@@ -0,0 +1,55 @@
+const todoForm = document.querySelector("#todo-form")
+const todoList = document.querySelector("#todo-list")
+const todoInput = todoForm.querySelector("input")
+
+const TODOS_KEY ="toDos"
+
+let toDos = []; 
+//const->let to update! if not, it doesn't store old todoList
+
+function saveTodos(){
+    localStorage.setItem("toDos",JSON.stringify(toDos))
+    //JSON.parse(toDos) -> back to list from string
+    //JSON.parse(localStorage.getItem("toDOs"))
+}
+
+function deleteTodo(event){
+   //console.log(event.target.parentElement.innerText)
+   const li = event.target.parentElement;
+   li.remove()
+}
+
+function paintTodo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+ 
+    const button = document.createElement("button");
+    button.innerText="❌";
+    button.addEventListener("click", deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
+
+    todoList.appendChild(li);
+}
+
+function handleTodoSubmit(event){
+    event.preventDefault();
+    const newTodo = todoInput.value;
+   // console.log(newTodo)
+    //localStorage only saves tubple not list
+    toDos.push(newTodo)
+    paintTodo(newTodo);
+    todoInput.value = "";
+    saveTodos();
+}
+
+todoForm.addEventListener("submit",handleTodoSubmit)
+
+const savedTodos = localStorage.getItem(TODOS_KEY) //string
+
+if(savedTodos !==null){
+    const parsedToDos = JSON.parse(savedTodos); //array
+    toDos = parsedToDos; //get old todoList
+    parsedToDos.forEach(paintTodo);
+}
